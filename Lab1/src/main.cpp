7371e78d380af49e99aaa235c1c5a574e8f1f343
CHONRADA KONGMUANG <chonrada.k@mail.kmutt.ac.th>
2020-02-29 22:03:51 +0700

    update comment

--- Lab1/src/main.cpp
+++ Lab1/src/main.cpp
@@ -2,13 +2,17 @@
 #include <util/delay.h>
 
 int main(){
+  // ddrd = 0000 0000 | 0000 0100 = 0000 0100 -> portd2 = output
   DDRD |= (1 << PORTD2);
+  // portd = 0000 0000 | 1111 1011 = 0000 0000 -> portd2 = low
   PORTD &= ~(1 << PORTD2);
 
   while (true)
   {
+    // portd2 high
     PORTD |= (1 << PORTD2);
     _delay_ms(1000);
+    // portd2 low
     PORTD |= ~(1 << PORTD2);
     _delay_ms(1000);
   }
